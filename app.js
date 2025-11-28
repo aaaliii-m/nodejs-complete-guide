4d841c06e436da5befb7e45e10c3e63498c306c1
ALI <aaaliii4u@yahoo.com>
2025-11-27 23:28:20 -0500

    createda simple response against a simple request

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    console.log(req);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Response Content</title></head>');
+    res.write('<body><h1>Hello, this is my first Nodejs application.</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
