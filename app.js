1688e51a0ec7cf00c335be4dbcb0155730f3b746
ALI <aaaliii4u@yahoo.com>
2025-11-29 00:09:53 -0500

    learned to route the requests

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+
+    if (req.url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>User Input</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="input-name" placeholder="Enter your message" /><button type="submit">Submit</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Response Content</title></head>');
-    res.write('<body><h1>Hello, this is my first Nodejs application.</h1></body>');
+    res.write('<body><h1>Congratulations, You Won!</h1></body>');
     res.write('</html>');
     res.end();
 });
