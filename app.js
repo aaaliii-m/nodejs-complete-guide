49bb786c67fefe7f41f2b6a3005f7b77b9a60be2
ALI <aaaliii4u@yahoo.com>
2025-11-29 00:30:32 -0500

    learned to redirect the requests

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
-    if (req.url === '/') {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>User Input</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="input-name" placeholder="Enter your message" /><button type="submit">Submit</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if (req = '/message' && method === 'POST') {
+        fs.writeFileSync('./message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('location', '/');
         return res.end();
     }
 
